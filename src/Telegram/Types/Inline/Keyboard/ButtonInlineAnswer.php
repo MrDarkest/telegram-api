44589d63966ac57de274162b0cebc879fc68276c
MrDarkest <lsirri@gmail.com>
2019-10-17 18:10:23 +0200

    Fix BUTTON_TYPE_INVALID for AnswerInlineQuery buttons

--- src/Telegram/Types/Inline/Keyboard/ButtonInlineAnswer.php
+++ src/Telegram/Types/Inline/Keyboard/ButtonInlineAnswer.php
@@ -6,7 +6,6 @@ namespace unreal4u\TelegramAPI\Telegram\Types\Inline\Keyboard;
 
 use unreal4u\TelegramAPI\Abstracts\TelegramTypes;
 use unreal4u\TelegramAPI\Telegram\Types\CallbackGame;
-use unreal4u\TelegramAPI\Telegram\Types\LoginUrl;
 
 /**
  * This object represents one button of an inline keyboard. You must use exactly one of the optional fields
@@ -14,14 +13,13 @@ use unreal4u\TelegramAPI\Telegram\Types\LoginUrl;
  * Note: This will only work in Telegram versions released after 9 April, 2016. Older clients will display unsupported
  * message.
  *
- *  WARNING: This type of button is NOT made for reply_markup of InlineQueryResult classes because Telegram don't want "callback_game" and "pay" properties in this case.
- * Use ButtonInlineAnswer instead.
+ * Objects defined as-is in october 2019
  *
- * Objects defined as-is july 2016. Updated in october 2019
+ * WARNING: This type of button is made for reply_markup of InlineQueryResult classes because Telegram don't want "callback_game" and "pay" properties in this case.
  *
  * @see https://core.telegram.org/bots/api#inlinekeyboardbutton
  */
-class Button extends TelegramTypes
+class ButtonInlineAnswer extends TelegramTypes
 {
     /**
      * Label text on the button
@@ -34,12 +32,6 @@ class Button extends TelegramTypes
      * @var string
      */
     public $url = '';
-
-    /**
-     * Optional. An HTTP URL used to automatically authorize the user.
-     * @var LoginUrl
-     */
-    public $login_url;
 
     /**
      * Optional. Data to be sent in a callback query to the bot when button is pressed, 1-64 bytes
@@ -71,30 +63,11 @@ class Button extends TelegramTypes
      */
     public $switch_inline_query_current_chat = '';
 
-    /**
-     * Optional. Description of the game that will be launched when the user presses the button.
-     *
-     * NOTE: This type of button must always be the first button in the first row.
-     *
-     * @var CallbackGame
-     */
-    public $callback_game;
-
-    /**
-     * Optional. Specify True, to send a Pay button.
-     *
-     * NOTE: This type of button must always be the first button in the first row.
-     * @var bool
-     */
-    public $pay = false;
-
     protected function mapSubObjects(string $key, array $data): TelegramTypes
     {
         switch ($key) {
             case 'login_url':
                 return new LoginUrl($data, $this->logger);
-            case 'callback_game':
-                return new CallbackGame($data, $this->logger);
         }
 
         return parent::mapSubObjects($key, $data);
